b810ccd89379d18d476983f8269bdb1c4341482c
VladShadrin <vladshadrin0806@gmail.com>
2025-03-05 15:25:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.getElementById("playButton").addEventListener("click", function() {
-    let audio = document.getElementById("audio");
-    audio.play().catch(e => console.log("Ошибка при воспроизведении: " + e));
+  let audio = document.getElementById("audio");
+  audio.play().catch(error => {
+    console.log("Ошибка воспроизведения аудио:", error);
+  });
 });
